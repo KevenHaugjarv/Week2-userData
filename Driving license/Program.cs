5fa99f35196b22e2a5886e5fc9f13d5f40c14973
unknown <khaugjar@tktk.ee>
2022-02-05 19:11:57 +0200

    control flow

---
+++ Driving license/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Driving_license
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanust;
+            //kui kasutaja on noorem, kui 18, siis kuvab konsoolis
+            // "oled liiga noor, et juhilube saada";
+            //Kui kasutaja on vanem, kui 18, siis programm kuvab konsoolis
+            //"oled piisavalt vana, et juhilube saada";
+            // kui kasutaja on 18, siis programm kuvab konsoolis
+            //"Palju õnne nüüd sa saad ka juhilube taotleda";
+
+            Console.WriteLine("Palun sisesta oma sünniaasta!");
+            int yearofBirth = Convert.ToInt32(Console.ReadLine());
+            int userAge = 2022 - yearofBirth;
+            Console.WriteLine($"Oled {userAge} aastat vana");
+
+            if (userAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana, et juhiluba saada");
+
+            }
+            else if (userAge < 18)
+            {
+                Console.WriteLine("Oled liiga noor, et juhilube saada");
+            }
+            else
+            {
+                Console.WriteLine("Palju õnne nüüd sa saad ka juhilube taotleda");
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
---
+++ UserData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat;
+            //programm arvutab kasutaja vanust;
+            //programm kuvab kasutaja vanust konsoolist;
+
+            Console.WriteLine("Palun sisesta oma sünniaasta");
+            int yearofBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2022 - yearofBirth;
+            //Console.WriteLine("Oled " + userAge + " aastat vana");
+            // string iterpolation
+
+            Console.WriteLine($"Oled {userAge} aastat vana");
+
+
+
+
+
+
+
+       
+        }
+    }
+}
